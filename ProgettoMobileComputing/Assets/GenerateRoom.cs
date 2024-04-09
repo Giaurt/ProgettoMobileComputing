eda0b58efd709a8492ed15b307467312e38e2158
Mid3x <giacomo.curtu@gmail.com>
2024-04-09 20:01:22 +0200

    SecondRoom&InfiniteRoomGenerator

--- ProgettoMobileComputing/Assets/GenerateRoom.cs
+++ ProgettoMobileComputing/Assets/GenerateRoom.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GenerateRoom : MonoBehaviour
 {
+    public GameObject roomPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,10 @@ public class GenerateRoom : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Y)){
+            Transform spawn = gameObject.transform.GetChild(0).transform;
+            Instantiate(roomPrefab, spawn.position, Quaternion.identity);
+        }
         
     }
 }
---
+++ ProgettoMobileComputing/Assets/SpawnKey.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnKey : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    
+    void Start()
+    {
+        SpawnKey();
+    }
+
+    void SpawnKey(){
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
